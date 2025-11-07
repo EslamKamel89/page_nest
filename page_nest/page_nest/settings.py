7a6cf1d0b04f00eda4a328006fc8b2aeb52bb038
EslamKamel89 <elsamkamelforex@gmail.com>
2025-11-07 10:53:29 +0200

    feat(django): integrate book_outlet app and configure static/templates
    - Register 'book_outlet' in INSTALLED_APPS
    - Add global 'templates' dir and 'static' dir to settings

--- page_nest/page_nest/settings.py
+++ page_nest/page_nest/settings.py
@@ -37,6 +37,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'book_outlet',
 ]
 
 MIDDLEWARE = [
@@ -54,7 +55,9 @@ ROOT_URLCONF = 'page_nest.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': [
+            BASE_DIR / 'templates'
+            ],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
@@ -120,3 +123,5 @@ STATIC_URL = 'static/'
 # https://docs.djangoproject.com/en/5.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+STATICFILES_DIRS = [BASE_DIR/'static']
