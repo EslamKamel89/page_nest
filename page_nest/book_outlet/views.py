738bc4ee6bfae4f0d339d9c1f34eaf437924df7a
EslamKamel89 <elsamkamelforex@gmail.com>
2025-11-08 16:26:37 +0200

    feat(book_outlet): implement book listing with ratings UI
    - Fetch all books and render with star ratings using Tailwind-styled template

--- page_nest/book_outlet/views.py
+++ page_nest/book_outlet/views.py
@@ -1,6 +1,12 @@
 from django.http import HttpRequest, HttpResponse
 from django.shortcuts import render
 
+from .models import Book
 
-async def index(request: HttpRequest)->HttpResponse:
-    return render(request , 'book_outlet/index.html')
+
+def index(request: HttpRequest)->HttpResponse:
+    books= Book.objects.all()
+    return render(request , 'book_outlet/index.html' , {
+        'books' : books ,
+        'star_range' : range(5)
+    })
