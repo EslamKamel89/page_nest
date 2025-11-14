857bc196bde3a9ab9a247bbf0bd316be0190022d
EslamKamel89 <elsamkamelforex@gmail.com>
2025-11-14 11:32:17 +0200

    feat(book_outlet): add Country model and seed published countries for books

--- page_nest/book_outlet/utils/seed_data.py
+++ page_nest/book_outlet/utils/seed_data.py
@@ -1,13 +1,20 @@
 from django.utils.text import slugify
 from faker import Faker
 
-from ..models import Address, Author, Book
+from ..models import Address, Author, Book, Country
 
 faker = Faker()
-def seed_data(authors_count:int = 10 , books_per_author:int =10):
-    Author.objects.delete()
-    Address.objects.delete()
-    Book.objects.delete()
+def seed_data(authors_count:int = 10 , books_per_author:int =10 , countries_count:int = 10 , countries_per_book :int= 3):
+    Author.objects.all().delete()
+    Address.objects.all().delete()
+    Book.objects.all().delete()
+    Country.objects.all().delete()
+    for _ in range(countries_count) :
+        Country.objects.create(
+            name=faker.country() ,
+            code=faker.country_code()
+        )
+    countries = Country.objects.all()
     for _ in range(authors_count):
         address = Address.objects.create(
             street=faker.street_address() ,
@@ -21,7 +28,7 @@ def seed_data(authors_count:int = 10 , books_per_author:int =10):
         )
         for _ in range(books_per_author):
             title = faker.sentence(nb_words=4).rstrip('.')
-            Book.objects.create(
+            book = Book.objects.create(
                 title=title ,
                 description= faker.paragraph(nb_sentences=4) ,
                 author=author  ,
@@ -29,4 +36,6 @@ def seed_data(authors_count:int = 10 , books_per_author:int =10):
                 is_bestselling=faker.boolean(chance_of_getting_true=50) ,
                 slug=slugify(title)
             )
+            book.published_countries.set(faker.random_choices(countries , countries_per_book))
+
     print(f"{authors_count} authors created and each author have {books_per_author} books created")
